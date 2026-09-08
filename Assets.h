b0597e37d381afbada1ef78189ccaa27d84f7efb
Ashley Fajardo <a.e.fajardo06@gmail.com>
2026-09-08 16:57:41 -0600

    crear lib de assets para ui

---
+++ Assets.h
@@ -0,0 +1,139 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include <string>
+
+using namespace std;
+
+// -----------------------------------------------------------------------------
+// AVATARES
+// -----------------------------------------------------------------------------
+const string AVATAR_1  = "assets/avatars/avatar_1.png";
+const string AVATAR_2  = "assets/avatars/avatar_2.png";
+const string AVATAR_3  = "assets/avatars/avatar_3.png";
+const string AVATAR_4  = "assets/avatars/avatar_4.png";
+const string AVATAR_5  = "assets/avatars/avatar_5.png";
+const string AVATAR_6  = "assets/avatars/avatar_6.png";
+const string AVATAR_7  = "assets/avatars/avatar_7.png";
+const string AVATAR_8  = "assets/avatars/avatar_8.png";
+const string AVATAR_9  = "assets/avatars/avatar_9.png";
+const string AVATAR_10 = "assets/avatars/avatar_10.png";
+
+
+// -----------------------------------------------------------------------------
+// BADGES
+// -----------------------------------------------------------------------------
+const string BADGE_FIRST_RESCUE = "assets/badges/1.png";
+const string BADGE_MINE_TAMER = "assets/badges/2.png";
+const string BADGE_QUICK_FUSE = "assets/badges/3.png";
+const string BADGE_NINE_LIVES = "assets/badges/4.png";
+const string BADGE_BOMB_DISPOSAL_EXPERT = "assets/badges/5.png";
+const string BADGE_CLEAN_SWEEP = "assets/badges/6.png";
+
+
+// -----------------------------------------------------------------------------
+// BOTONES DE ACCION
+// -----------------------------------------------------------------------------
+const string BOTON_ACCEPT = "assets/buttons/accept.png";
+const string BOTON_BACK = "assets/buttons/back.png";
+const string BOTON_CANCEL = "assets/buttons/cancel.png";
+const string BOTON_CONFIG = "assets/buttons/config.png";
+const string BOTON_FORWARD = "assets/buttons/forward.png";
+const string BOTON_HELP = "assets/buttons/help.png";
+const string BOTON_HIGH_SCORES = "assets/buttons/high-scores.png";
+const string BOTON_HOME = "assets/buttons/home.png";
+const string BOTON_INFO = "assets/buttons/info.png";
+const string BOTON_MUSIC = "assets/buttons/music.png";
+const string BOTON_RETRY = "assets/buttons/retry.png";
+const string BOTON_REWARDS = "assets/buttons/rewards.png";
+const string BOTON_SAVE = "assets/buttons/save.png";
+
+
+// -----------------------------------------------------------------------------
+// TABLERO DE JUEGO
+// -----------------------------------------------------------------------------
+const string CELDA_1 = "assets/dashboard/1.png";
+const string CELDA_2 = "assets/dashboard/2.png";
+const string CELDA_3 = "assets/dashboard/3.png";
+const string CELDA_4 = "assets/dashboard/4.png";
+const string CELDA_5 = "assets/dashboard/5.png";
+const string CELDA_BOMBA = "assets/dashboard/bomb.png";
+const string CELDA_LIBRE = "assets/dashboard/clear.png";
+const string CELDA_BOMBA_EXPLOTADA = "assets/dashboard/landed_bomb.png";
+const string CELDA_MARCADA = "assets/dashboard/marked.png";
+const string CELDA_OCULTA = "assets/dashboard/tile.png";
+
+
+// -----------------------------------------------------------------------------
+// FUENTES
+// -----------------------------------------------------------------------------
+const string FONT_ALICE = "assets/fonts/Alice-Regular.ttf";
+const string FONT_ANTONIO = "assets/fonts/Antonio-VariableFont_wght.ttf";
+const string FONT_LOBSTER = "assets/fonts/Lobster-Regular.ttf";
+
+
+// -----------------------------------------------------------------------------
+// MENU PRINCIPAL
+// -----------------------------------------------------------------------------
+const string MENU_CLAIM_REWARD = "assets/menu/claim-reward.png";
+const string MENU_EXIT = "assets/menu/exit.png";
+const string MENU_HIGH_SCORES = "assets/menu/high-scores.png";
+const string MENU_HOW_TO_PLAY = "assets/menu/how-to-play.png";
+const string MENU_MY_PROFILE = "assets/menu/my-profile.png";
+const string MENU_PLAY = "assets/menu/play.png";
+
+
+// -----------------------------------------------------------------------------
+// MUSICA
+// -----------------------------------------------------------------------------
+const string MUSICA_CHICKEN_FEET = "assets/music/Chicken-Feet-JamieBerry.mp3";
+const string MUSICA_IT_AINT_RIGHT = "assets/music/It-Aint-Right-JamieBerry.mp3";
+const string MUSICA_THE_MINOR_DRAGS = "assets/music/The-Minor-Drags-Lazlo.mp3";
+
+
+// -----------------------------------------------------------------------------
+// TEMPLATES DE UI
+// -----------------------------------------------------------------------------
+const string TEMPLATE_BADGES_INFO = "assets/templates/badges-info.png";
+const string TEMPLATE_BLACKPOWDER_BAY_MAP = "assets/templates/blackpowder-bay-map.png";
+const string TEMPLATE_BOX = "assets/templates/box-template.png";
+const string TEMPLATE_CALAMITY_CAYS_MAP = "assets/templates/calamity-cays-map.png";
+const string TEMPLATE_CLEAN_1 = "assets/templates/clean-template-1.png";
+const string TEMPLATE_CLEAN_2 = "assets/templates/clean-template-2.png";
+const string TEMPLATE_CLEAN_3 = "assets/templates/clean-template-3.png";
+const string TEMPLATE_FELIX = "assets/templates/felix-template.png";
+const string TEMPLATE_HOW_TO_PLAY_1 = "assets/templates/how-to-play-1.png";
+const string TEMPLATE_HOW_TO_PLAY_2 = "assets/templates/how-to-play-2.png";
+const string TEMPLATE_HOW_TO_PLAY_3 = "assets/templates/how-to-play-3.png";
+const string TEMPLATE_LEVEL_FAILED = "assets/templates/level-failed.png";
+const string TEMPLATE_LEVEL_WON = "assets/templates/level-won.png";
+const string TEMPLATE_MENU_PRINCIPAL = "assets/templates/menu-principal.png";
+
+
+// -----------------------------------------------------------------------------
+// VIDEOS DE BOSSES
+// -----------------------------------------------------------------------------
+const string VIDEO_CAPTAIN_BRINEYBEARD = "assets/bosses/Captain-Brineybeard.mp4";
+const string VIDEO_BEPPI_THE_CLOWN = "assets/bosses/Beppi-the-Clown.mp4";
+const string VIDEO_GRIM_MATCHSTICK = "assets/bosses/Grim-Matchstick.mp4";
+const string VIDEO_RIBBY_AND_CROAKS = "assets/bosses/Ribby-&-Croaks.mp4";
+const string VIDEO_CALA_MARIA = "assets/bosses/Cala-Maria.mp4";
+const string VIDEO_DR_KAHL_ROBOT = "assets/bosses/Dr.-Kahl's-Robot.mp4";
+const string VIDEO_WALLY_WARBLES = "assets/bosses/Wally-Warbles.mp4";
+const string VIDEO_CHEF_SALTBAKER = "assets/bosses/Chef-Saltbaker.mp4";
+const string VIDEO_THE_DEVIL = "assets/bosses/The-Devil.mp4";
+const string VIDEO_HILDA_BERG = "assets/bosses/Hilda-Berg.mp4";
+const string VIDEO_WERNER_WERMAN = "assets/bosses/Werner-Werman.mp4";
+const string VIDEO_BARONESS_VON_BON_BON = "assets/bosses/Baroness-Von-Bon-Bon.mp4";
+const string VIDEO_DJIMMI_THE_GREAT = "assets/bosses/Djimmi-the-Great.mp4";
+const string VIDEO_KING_DICE = "assets/bosses/King-Dice.mp4";
+
+
+// -----------------------------------------------------------------------------
+// ANIMACIONES GENERALES
+// -----------------------------------------------------------------------------
+const string ANIMACION_INTRO = "assets/animations/intro.mp4";
+const string ANIMACION_JULIUS = "assets/animations/julius.mp4";
+const string ANIMACION_TO_THE_RESCUE = "assets/animations/to-the-rescue.mp4";
+
+#endif
